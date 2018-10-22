152e73d13cb3dd4a8267519bb04682c3da56323c
tanc <mail@tanc.me>
2018-10-22 11:21:27 +0200

    Removes old fasd init

--- .config/fish/conf.d/fasd.init.fish
+++
@@ -1,9 +0,0 @@
-# Detect fasd
-if type -q fasd
-  # Hook into fish preexec event
-  function __fasd_run -e fish_preexec
-    command fasd --proc (command fasd --sanitize "$argv" | tr -s ' ' \n) > "/dev/null" 2>&1 &
-  end
-else
-  echo "🍒  Please install 'fasd' first!"
-end
